4aebf1827daa15e2736c9f4970cae45c8cafae4d
freddyflipflop <55545041+freddyflipflop@users.noreply.github.com>
2022-07-30 15:45:13 +0530

    Add files via upload

---
+++ api.py
@@ -0,0 +1,49 @@
+from urllib import response
+import requests
+import os
+import json
+url_detect = ""
+
+def switch(alcohol_content):
+    okay_msg = "Happy Journey"
+    not_okay_msg = "Stay where you are"
+    state_on = "HIGH"
+    state_off = "LOW"
+    url = "https://cloud.boltiot.com/remote/APIKEY/digitalWrite"
+    headers = {
+        'Cache-Control': "no-cache"
+        }
+    if(alcohol_content < 800):
+        querystring = {"pin":"0","state":state_on,"deviceName":"DEVICENAME"}
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        print(response.text)
+        print(okay_msg)
+    elif(alcohol_content > 800):
+        querystring = {"pin":"0","state":state_off,"deviceName":"DEVICENAME"}
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        print(response.text)
+        print(not_okay_msg)
+
+def connect_to_bolt(url_detect):
+    response = requests.get(url_detect)
+    data=response.json()
+    print(data)
+    #return data
+    alcohol_content = int(data["value"])
+    switch(alcohol_content)
+if __name__ == "__main__":
+    connect_to_bolt(url_detect)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
